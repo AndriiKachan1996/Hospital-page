d94da616a29ffef30759342c1164cea3ecb6c6e4
andrii <realandryxa1996@gmail.com>
2023-04-06 22:37:30 +0300

    6

--- js/menu.js
+++ js/menu.js
@@ -25,21 +25,3 @@
     document.body.style.overflow = "auto";
   });
 })();
-
-function slidesPlugin(activeSlide) {
-  const slides = document.querySelectorAll(".slide");
-  slides[activeSlide].classList.add("active");
-  slides.forEach((slide) => {
-    slide.addEventListener("click", () => {
-      clearActiveClasses();
-      slide.classList.add("active");
-    });
-  });
-
-  function clearActiveClasses() {
-    slides.forEach((slide) => {
-      slide.classList.remove("active");
-    });
-  }
-}
-slidesPlugin(4);
